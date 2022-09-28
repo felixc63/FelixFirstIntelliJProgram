b95e1340af20a49e415ff0ecacbbeee3c48dc8c5
BT_3W2_04 <felixc63@nycstudents.net>
2022-09-28 11:44:09 -0400

    Code Changed

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class HelloWorld {
+import java.util.Scanner;
+
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Yo! What's your name? ");
+        String name = scan.nextLine();
+
+        System.out.println("Hello "+ name + "!");
+    }
 }
